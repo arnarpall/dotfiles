6891b4d460e55560e8211d2129909eda71bb2259
Arnar Páll <arnarp.arnarsson@upwind.io>
2023-12-01 13:16:44 +0000

    Adding neovim setup for go and rust

---
+++ config/nvim/custom/configs/lspconfig.lua
@@ -0,0 +1,56 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+
+local lspconfig = require("lspconfig")
+local util = require "lspconfig/util"
+
+lspconfig.gopls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = {"gopls"},
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      completeUnimported = true,
+      usePlaceholders = true,
+      analyses = {
+        unusedparams = true,
+      },
+    },
+  },
+}
+
+lspconfig.rust_analyzer.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "rust" },
+  root_dir = util.root_pattern("Cargo.toml"),
+  settings = {
+    ['rust-analyzer'] = {
+      cargo = {
+        allFeatures = true,
+      },
+    },
+  },
+}
+
+
+
+--[[
+lspconfig.rust_analyser.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { "/Users/arnar/.cargo/bin/rust-analyzer" },
+  filetypes = { "rust" },
+  root_dir = util.root_pattern("Cargo.toml"),
+  settings = {
+    ["rust-analyzer"]  = {
+      cargo = {
+        allFeatures = true,
+      },
+    },
+  },
+}
+--]]
